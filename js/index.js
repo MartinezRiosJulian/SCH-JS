33b6d6afdb83fa8918d282fd63a2c85078656d74
Martinez Rios Julian <julian.martinez62913@gmail.com>
2022-08-10 18:21:01 -0300

    primer pre-entrega

--- js/index.js
+++ js/index.js
@@ -1,3 +1,28 @@
+let arrayRemeras = [
+  { id: 0, nombre: "Remera", talle: "S", precio: 100 },
+  { id: 1, nombre: "Remera2", talle: "M", precio: 20 },
+  { id: 2, nombre: "Remera3", talle: "L", precio: 80 },
+  { id: 3, nombre: "Remera4", talle: "XL", precio: 200 },
+];
+let arrayGorras = [
+  { id: 0, nombre: "Gorra", talle: "S", precio: 100 },
+  { id: 2, nombre: "Gorra3", talle: "M", precio: 80 },
+  { id: 1, nombre: "Gorra2", talle: "L", precio: 20 },
+  { id: 3, nombre: "Gorra4", talle: "XL", precio: 200 },
+];
+let arrayTazas = [
+  { id: 0, nombre: "Taza", talle: "N/NA", precio: 100 },
+  { id: 1, nombre: "Taza2", talle: "N/NA", precio: 20 },
+  { id: 2, nombre: "Taza3", talle: "N/NA", precio: 80 },
+  { id: 3, nombre: "Taza4", talle: "N/NA", precio: 200 },
+];
+let arrayStickers = [
+  { id: 0, nombre: "Sticker", talle: "N/NA", precio: 100 },
+  { id: 1, nombre: "Sticker2", talle: "N/NA", precio: 20 },
+  { id: 2, nombre: "Sticker3", talle: "N/NA", precio: 80 },
+  { id: 3, nombre: "Sticker4", talle: "N/NA", precio: 200 },
+];
+
 function saludar() {
   alert("Bienvenid@ a SCH Mecanica Integral");
 }
@@ -10,40 +35,101 @@ function selecProd() {
 
 selecProd();
 
-let producto = prompt("Llene su carrito");
-if (
-  producto != "" &&
-  (producto == "TAZA SCH" || producto == "Taza SCH" || producto == "taza sch")
-) {
-  alert("Carrito Taza SCH");
-} else if (
-  producto != "" &&
-  (producto == "GORRA SCH" || producto == "Gorra SCH" || producto == "gorra sch")
-) {
-  alert("Carrito Gorra SCH");
-} else if (
-  producto != "" &&
-  (producto == "REMERA SCH" || producto == "Remera SCH" || producto == "remera sch")
-) {
-  alert("Carrito Remera SCH");
-} else if (
-  producto != "" &&
-  (producto == "STICKER SCH" || producto == "Sticker SCH" || producto == "sticker sch")
-) {
-  alert("Carrito Sticker SCH");
-}
-else {
-  alert ("Carrito Vacio")
+
+let producto = "";
+
+while (producto != "Aceptar") {
+  producto = prompt("Taza|Remera|Gorra|Sticker|Aceptar");
+  switch (producto) {
+    case "Taza":
+      comprarTaza();
+      break;
+    case "Remera":
+      comprarRemera();
+      break;
+    case "Gorra":
+      comprarGorra();
+      break;
+    case "Sticker":
+      comprarSticker();
+      break;
+    case "Aceptar":
+      break;
+    default:
+      alert("Carrito vacio");
+      break;
+  }
+  alert("Procesando su compra");
+  let carga = 3;
+  while (carga >= 0) {
+    carga--;
+    alert(carga);
+  }
+  
+  alert("Compra Finalizada");
 }
 
-alert("Procesando su compra");
-
-let carga = 3;
-while (carga <= 3) {
-  carga--;
-  alert(carga);
-  if (carga == 0) {
-    alert("Compra Finalizada");
-    break;
+function comprarTaza() {
+  let productoElegido = prompt("Elija su Taza");
+  const tazaBuscada = arrayTazas.find((taza) => {
+    return taza.nombre == productoElegido;
+  });
+  if (tazaBuscada) {
+    console.log("Comprado");
+    console.log(tazaBuscada);
+  } else {
+    console.log("No tenemos ese producto");
   }
 }
+
+function comprarRemera() {
+  let productoElegido = prompt("Elija su Remera");
+  const remeraBuscada = arrayRemeras.find((remera) => {
+    return remera.nombre == productoElegido;
+  });
+  if (remeraBuscada) {
+    console.log("Comprado");
+    console.log(remeraBuscada);
+  } else {
+    console.log("No tenemos ese producto");
+  }
+}
+
+function comprarGorra() {
+  let productoElegido = prompt("Elija su Gorra");
+  const gorraBuscada = arrayGorras.find((gorra) => {
+    return gorra.nombre == productoElegido;
+  });
+  if (gorraBuscada) {
+    console.log("Comprado");
+    console.log(gorraBuscada);
+  } else {
+    console.log("No tenemos ese producto");
+  }
+}
+
+function comprarSticker() {
+  let productoElegido = prompt("Elija su Sticker");
+  const stickerBuscado = arrayStickers.find((sticker) => {
+    return sticker.nombre == productoElegido;
+  });
+  if (stickerBuscado) {
+    console.log("Comprado");
+    console.log(stickerBuscado);
+  } else {
+    console.log("No tenemos ese producto");
+  }
+}
+
+//let producto = prompt("Llene su carrito");
+/*if (producto.toLowerCase() == 'taza') {
+  alert('Agregado a carrito "Taza SCH"');
+} else if (producto.toLowerCase() == 'gorra') {
+  alert('Agregado a carrito "Gorra SCH"');
+} else if (producto.toLowerCase() == 'remera') {
+  alert('Agregado a carrito "Remera SCH"');
+} else if (producto.toLowerCase() == 'sticker') {
+  alert('Agregado a carrito "Sticker SCH"');
+} else {
+  alert('Carrito vacio');
+}*/
