4c38ec3a49489dd0279bce823562b2d4dafbf684
Martinez Rios Julian <julian.martinez62913@gmail.com>
2022-08-24 23:12:46 -0300

    rama prueba storage

---
+++ js/arrays.js
@@ -0,0 +1,58 @@
+export let productos = [
+  { id: 0, nombre: "Remera /algodon" },
+  { id: 1, nombre: "Remera /mangaLarga" },
+  { id: 2, nombre: "Remera /bordada" },
+  { id: 3, nombre: "Remera /poliester" },
+  { id: 4, nombre: "Tazac c/blanco" },
+  { id: 5, nombre: "Tazac c/negro" },
+  { id: 6, nombre: "Tazac c/naranja" },
+  { id: 7, nombre: "Tazac c/azul" },
+  { id: 8, nombre: "Gorra c/azul" },
+  { id: 9, nombre: "Gorra c/naranja" },
+  { id: 10, nombre: "Gorra c/negro" },
+  { id: 11, nombre: "Gorra c/blanco" },
+  { id: 12, nombre: "Sticker auto" },
+  { id: 13, nombre: "Sticker termo" },
+  { id: 14, nombre: "Sticker mochila" },
+  { id: 15, nombre: "Sticker /parche" },
+];
+export let talles = [
+  { id: 0, talle: "S" },
+  { id: 1, talle: "M" },
+  { id: 2, talle: "L" },
+  { id: 3, talle: "XL" },
+];
+export let precios = [
+  { id: 0, precio: "100" },
+  { id: 1, precio: "150" },
+  { id: 2, precio: "50" },
+  { id: 3, precio: "250" },
+];
+
+export let listaArrays = productos.concat(talles, precios);
+
+let arrayRemeras = [
+  { id: 0, nombre: "Remera /algodon", talle: "S", precio: 100 },
+  { id: 1, nombre: "Remera /poliester", talle: "M", precio: 20 },
+  { id: 2, nombre: "Remera /bordada", talle: "L", precio: 80 },
+  { id: 3, nombre: "Remera /mangaLarga", talle: "XL", precio: 200 },
+];
+let arrayGorras = [
+  { id: 0, nombre: "Gorra c/blanco", talle: "S", precio: 150 },
+  { id: 2, nombre: "Gorra c/negro", talle: "M", precio: 90 },
+  { id: 1, nombre: "Gorra c/naranja", talle: "L", precio: 20 },
+  { id: 3, nombre: "Gorra c/azul", talle: "XL", precio: 210 },
+];
+let arrayTazas = [
+  { id: 0, nombre: "Taza c/blanco", talle: "", precio: 120 },
+  { id: 1, nombre: "Taza c/negro", talle: "", precio: 30 },
+  { id: 2, nombre: "Taza c/naranja ", talle: "", precio: 40 },
+  { id: 3, nombre: "Taza c/azul", talle: "", precio: 70 },
+];
+let arrayStickers = [
+  { id: 0, nombre: "Sticker auto", talle: "", precio: 130 },
+  { id: 1, nombre: "Sticker termo", talle: "", precio: 60 },
+  { id: 2, nombre: "Sticker mochila", talle: "", precio: 35 },
+  { id: 3, nombre: "Sticker /parche", talle: "", precio: 45 },
+];
+export let listaArrays2 = arrayRemeras.concat(arrayGorras, arrayTazas, arrayStickers);
--- js/index.js
+++ js/index.js
@@ -1,132 +1,6 @@
-let arrayRemeras = [
-  { id: 0, nombre: "Remera", talle: "S", precio: 100 },
-  { id: 1, nombre: "Remera2", talle: "M", precio: 20 },
-  { id: 2, nombre: "Remera3", talle: "L", precio: 80 },
-  { id: 3, nombre: "Remera4", talle: "XL", precio: 200 },
-];
-let arrayGorras = [
-  { id: 0, nombre: "Gorra", talle: "S", precio: 100 },
-  { id: 2, nombre: "Gorra3", talle: "M", precio: 80 },
-  { id: 1, nombre: "Gorra2", talle: "L", precio: 20 },
-  { id: 3, nombre: "Gorra4", talle: "XL", precio: 200 },
-];
-let arrayTazas = [
-  { id: 0, nombre: "Taza", talle: "N/NA", precio: 100 },
-  { id: 1, nombre: "Taza2", talle: "N/NA", precio: 20 },
-  { id: 2, nombre: "Taza3", talle: "N/NA", precio: 80 },
-  { id: 3, nombre: "Taza4", talle: "N/NA", precio: 200 },
-];
-let arrayStickers = [
-  { id: 0, nombre: "Sticker", talle: "N/NA", precio: 100 },
-  { id: 1, nombre: "Sticker2", talle: "N/NA", precio: 20 },
-  { id: 2, nombre: "Sticker3", talle: "N/NA", precio: 80 },
-  { id: 3, nombre: "Sticker4", talle: "N/NA", precio: 200 },
-];
-let listaArrays = arrayRemeras.concat(arrayGorras, arrayTazas, arrayStickers);
+import { productos, talles, precios } from "./arrays.js";
 
-function saludar() {
-  alert("Bienvenid@ a SCH Mecanica Integral");
-}
-
-saludar();
-
-let producto = "";
-
-while (producto != "Salir") {
-  producto = prompt(
-    "Seleccione su producto \n Taza|Remera|Gorra|Sticker|Ver|Salir"
-  );
-  switch (producto) {
-    case "Taza":
-      comprarTaza();
-      break;
-    case "Remera":
-      comprarRemera();
-      break;
-    case "Gorra":
-      comprarGorra();
-      break;
-    case "Sticker":
-      comprarSticker();
-      break;
-    case "Ver":
-      verProductos();
-      break;
-    case "Salir":
-      break;
-    default:
-      alert("Carrito vacio");
-      break;
-  }
-}
-alert("Compra Finalizada");
-let carga = 3;
-while (carga > 0) {
-  carga--;
-  alert(carga);
-}
-
-function comprarTaza() {
-  let productoElegido = prompt("Elija su Taza");
-  const tazaBuscada = arrayTazas.find((taza) => {
-    return taza.nombre == productoElegido;
-  });
-  if (tazaBuscada) {
-    console.log("Comprado");
-    console.log(tazaBuscada);
-  } else {
-    console.log("No tenemos ese producto");
-  }
-}
-
-function comprarRemera() {
-  let productoElegido = prompt("Elija su Remera");
-  const remeraBuscada = arrayRemeras.find((remera) => {
-    return remera.nombre == productoElegido;
-  });
-  if (remeraBuscada) {
-    console.log("Comprado");
-    console.log(remeraBuscada);
-  } else {
-    console.log("No tenemos ese producto");
-  }
-}
-
-function comprarGorra() {
-  let productoElegido = prompt("Elija su Gorra");
-  const gorraBuscada = arrayGorras.find((gorra) => {
-    return gorra.nombre == productoElegido;
-  });
-  if (gorraBuscada) {
-    console.log("Comprado");
-    console.log(gorraBuscada);
-  } else {
-    console.log("No tenemos ese producto");
-  }
-}
-
-function comprarSticker() {
-  let productoElegido = prompt("Elija su Sticker");
-  const stickerBuscado = arrayStickers.find((sticker) => {
-    return sticker.nombre == productoElegido;
-  });
-  if (stickerBuscado) {
-    console.log("Comprado");
-    console.log(stickerBuscado);
-  } else {
-    console.log("No tenemos ese producto");
-  }
-}
-
-function verProductos() {
-  listaArrays.forEach((producto) => {
-    console.log(
-      `ID: ${producto.id} Nombre: ${producto.nombre} Talle: ${producto.talle} Precio: ${producto.precio}`
-    );
-  });
-}
-
-let submitButton = document.getElementById("submit-btn");
+let submitButton = document.querySelector("#submitBtn");
 submitButton.addEventListener("click", (event) => {
   event.preventDefault();
   mostrarValoresForm();
@@ -137,3 +11,26 @@ function mostrarValoresForm() {
     console.log(input.value);
   });
 }
+
+let submit = document.querySelector("#submitBtn");
+submit.addEventListener("click", (event) => {
+  event.preventDefault();
+  let msj = document.createElement("div");
+  msj.innerHTML += `<div> "Compra Finalizada"</div>`;
+  document.body.appendChild(msj);
+});
+
+productos.forEach(
+  (prod) =>
+    (selecProd.innerHTML += `<option value="${prod.nombre}">${prod.nombre}</option>`)
+);
+
+talles.forEach(
+  (talle) =>
+    (selecTalle.innerHTML += `<option value="${talle.talle}">${talle.talle}</option>`)
+);
+
+precios.forEach(
+  (precio) =>
+    (selecPrecio.innerHTML += `<option value="${precio.precio}">${precio.precio}</option>`)
+);
