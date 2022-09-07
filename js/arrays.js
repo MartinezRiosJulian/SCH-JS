31a6668e9274aba8cadcc5cbf81013a90a07369d
Martinez Rios Julian <julian.martinez62913@gmail.com>
2022-09-07 18:11:56 -0300

    Librerias

--- js/arrays.js
+++ js/arrays.js
@@ -1,20 +1,42 @@
+export let listaProductos = [
+  "Remera Algodon",
+  "Remera Poliester",
+  "Remera Manga Larga",
+  "Remera Bordada",
+  "Gorra Azul",
+  "Gorra Naranja",
+  "Gorra Negra",
+  "Gorra Blanca",
+  "Taza Blanca",
+  "Taza Negra",
+  "Taza Naranja",
+  "Taza Azul",
+  "Sticker Auto",
+  "Sticker Termo",
+  "Sticker Mochila",
+  "Sticker Parche"
+]
+export let listaTalles = ["XS", "S", "M", "L", "XL", "XXL", "N/NA"]
+
+export let listaPrecios = [500, 550, 600, 650, 700, 750, 800, 150]
+
 export let productos = [
-  { id: 0, nombre: "Remera /algodon" },
-  { id: 1, nombre: "Remera /mangaLarga" },
-  { id: 2, nombre: "Remera /bordada" },
-  { id: 3, nombre: "Remera /poliester" },
-  { id: 4, nombre: "Taza /blanco" },
-  { id: 5, nombre: "Taza /negro" },
-  { id: 6, nombre: "Taza /naranja" },
-  { id: 7, nombre: "Taza /azul" },
-  { id: 8, nombre: "Gorra /azul" },
-  { id: 9, nombre: "Gorra /naranja" },
-  { id: 10, nombre: "Gorra /negro" },
-  { id: 11, nombre: "Gorra /blanco" },
-  { id: 12, nombre: "Sticker auto" },
-  { id: 13, nombre: "Sticker termo" },
-  { id: 14, nombre: "Sticker mochila" },
-  { id: 15, nombre: "Sticker/parche" },
+  { id: 0, nombre: "Remera", tipo: "Algodon" },
+  { id: 1, nombre: "Remera", tipo: "Poliester" },
+  { id: 2, nombre: "Remera", tipo: "Manga Larga" },
+  { id: 3, nombre: "Remera", tipo: "Bordada" },
+  { id: 4, nombre: "Taza", color: "Blanca" },
+  { id: 5, nombre: "Taza", color: "Negra" },
+  { id: 6, nombre: "Taza", color: "Naranja" },
+  { id: 7, nombre: "Taza", color: "Azul" },
+  { id: 8, nombre: "Gorra", colorRopa: "Azul" },
+  { id: 9, nombre: "Gorra", colorRopa: "Naranja" },
+  { id: 10, nombre: "Gorra", colorRopa: "Negra" },
+  { id: 11, nombre: "Gorra", colorRopa: "Blanca" },
+  { id: 12, nombre: "Sticker", para: "Auto" },
+  { id: 13, nombre: "Sticker", para: "Termo" },
+  { id: 14, nombre: "Sticker", para: "Mochila" },
+  { id: 15, nombre: "Sticker", para: "Parche" },
 ];
 export let talles = [
   { id: 0, talle: "S" },
@@ -31,28 +53,58 @@ export let precios = [
 
 export let listaArrays = productos.concat(talles, precios);
 
-let arrayRemeras = [
-  { id: 0, nombre: "Remera /algodon", talle: "S", precio: 100 },
-  { id: 1, nombre: "Remera /poliester", talle: "M", precio: 20 },
-  { id: 2, nombre: "Remera /bordada", talle: "L", precio: 80 },
-  { id: 3, nombre: "Remera /mangaLarga", talle: "XL", precio: 200 },
+export let arraysdeRemeras = [
+  { id: 0, nombre: "Remera /algodon", precio: 100 },
+  { id: 1, nombre: "Remera /poliester", precio: 20 },
+  { id: 2, nombre: "Remera /bordada", precio: 80 },
+  { id: 3, nombre: "Remera /mangaLarga", precio: 200 },
 ];
-let arrayGorras = [
-  { id: 0, nombre: "Gorra c/blanco", talle: "S", precio: 150 },
-  { id: 2, nombre: "Gorra c/negro", talle: "M", precio: 90 },
-  { id: 1, nombre: "Gorra c/naranja", talle: "L", precio: 20 },
-  { id: 3, nombre: "Gorra c/azul", talle: "XL", precio: 210 },
+export let arraysdeGorras = [
+  { id: 0, nombre: "Gorra /blanco", precio: 150 },
+  { id: 2, nombre: "Gorra /negro", precio: 90 },
+  { id: 1, nombre: "Gorra /naranja", precio: 20 },
+  { id: 3, nombre: "Gorra /azul", precio: 210 },
 ];
-let arrayTazas = [
-  { id: 0, nombre: "Taza c/blanco", talle: "", precio: 120 },
-  { id: 1, nombre: "Taza c/negro", talle: "", precio: 30 },
-  { id: 2, nombre: "Taza c/naranja ", talle: "", precio: 40 },
-  { id: 3, nombre: "Taza c/azul", talle: "", precio: 70 },
+export let arraysdeTazas = [
+  { id: 0, nombre: "Taza /blanco", precio: 120 },
+  { id: 1, nombre: "Taza /negro", precio: 30 },
+  { id: 2, nombre: "Taza /naranja ", precio: 40 },
+  { id: 3, nombre: "Taza /azul", precio: 70 },
 ];
-let arrayStickers = [
-  { id: 0, nombre: "Sticker auto", talle: "", precio: 130 },
-  { id: 1, nombre: "Sticker termo", talle: "", precio: 60 },
-  { id: 2, nombre: "Sticker mochila", talle: "", precio: 35 },
-  { id: 3, nombre: "Sticker /parche", talle: "", precio: 45 },
+export let arraysdeStickers = [
+  { id: 0, nombre: "Sticker auto", precio: 130 },
+  { id: 1, nombre: "Sticker termo", precio: 60 },
+  { id: 3, nombre: "Sticker/parche", precio: 45 },
+  { id: 2, nombre: "Sticker mochila", precio: 35 },
 ];
-export let listaArrays2 = arrayRemeras.concat(arrayGorras, arrayTazas, arrayStickers);
+export let listaArrays2 = arraysdeRemeras.concat(
+  arraysdeGorras,
+  arraysdeTazas,
+  arraysdeStickers
+);
+
+export let listaRemeras = [
+  { id: 0, nombre: "Remera", tipo: "Algodon" },
+  { id: 1, nombre: "Remera", tipo: "Poliester" },
+  { id: 2, nombre: "Remera", tipo: "Manga Larga" },
+  { id: 3, nombre: "Remera", tipo: "Bordada" },
+]
+export let listaGorras = [
+  { id: 0, nombre: "Gorra", color: "azul" },
+  { id: 1, nombre: "Gorra", color: "naranja" },
+  { id: 2, nombre: "Gorra", color: "negra" },
+  { id: 3, nombre: "Gorra", color: "blanca" },
+]
+export let listaTazas = [
+  { id: 0, nombre: "Taza", color: "blanca" },
+  { id: 1, nombre: "Taza", color: "negra" },
+  { id: 2, nombre: "Taza", color: "naranja" },
+  { id: 3, nombre: "Taza", color: "azul" },
+]
+export let listaStickers = [
+  { id: 0, nombre: "Sticker", para: "auto" },
+  { id: 1, nombre: "Sticker", para: "termo" },
+  { id: 2, nombre: "Sticker", para: "mochila" },
+  { id: 3, nombre: "Sticker", para: "parche" },
+]
+export let productosTotales = listaRemeras.concat(listaGorras, listaTazas, listaStickers);
--- js/index.js
+++ js/index.js
@@ -1,73 +1,176 @@
-import { productos, talles, precios } from "./arrays.js";
+import { listaTalles, listaPrecios, listaProductos } from "./arrays.js";
 
-const localStorage = window.localStorage;
+const $selectProducto = document.getElementById("selecProd");
+const $selectTalle = document.getElementById("selecTalle");
+const $selectPrecio = document.getElementById("selecPrecio");
+const $submitBtn = document.getElementById("submitBtn");
+let objObtenido = localStorage.getItem("Compra");
+let msj = "Compra Finalizada";
+
+const clickBtn = (event) => {
+  event.preventDefault();
+  document.getElementById("compraFinal").innerText = msj;
+  Swal.fire({
+    title: 'Listo!',
+    text: 'Compra Finalizada',
+    icon: 'success',
+    confirmButtonText: 'Cerrar'
+  })
+};
+
+$submitBtn.addEventListener("click", clickBtn);
+$submitBtn.addEventListener("click", mostrarLocalStorage);
+$submitBtn.addEventListener("click", mostrarValoresForm);
+
+function mostrarSelectProd(arreglo, lugar) {
+  let option =
+    '<option value="" disabled selected>Seleccione producto</option>';
+
+  for (let i = 0; i < arreglo.length; i++) {
+    option += '<option value="' + arreglo[i] + '">' + arreglo[i] + "</option>";
+  }
+
+  lugar.innerHTML = option;
+}
+
+function mostrarSelectTalle(arreglo, lugar) {
+  let option =
+    '<option value="" disabled selected>Seleccione Talle</option>';
+
+  for (let i = 0; i < arreglo.length; i++) {
+    option += '<option value="' + arreglo[i] + '">' + arreglo[i] + "</option>";
+  }
+
+  lugar.innerHTML = option;
+}
+
+function mostrarSelectPrecio(arreglo, lugar) {
+  let option =
+    '<option value="" disabled selected>$</option>';
+
+  for (let i = 0; i < arreglo.length; i++) {
+    option += '<option value="' + arreglo[i] + '">' + arreglo[i] + "</option>";
+  }
+
+  lugar.innerHTML = option;
+}
+
+mostrarSelectProd(listaProductos, $selectProducto);
+mostrarSelectTalle(listaTalles, $selectTalle);
+mostrarSelectPrecio(listaPrecios, $selectPrecio);
+
+function cortar(array, inicio, fin, lugar) {
+  let cortar = array.slice(inicio, fin);
+  mostrarSelectProd(cortar, lugar);
+  mostrarSelectTalle(cortar, lugar);
+  mostrarSelectPrecio(cortar, lugar);
+}
+
+$selectProducto.addEventListener("change", function () {
+  const valorProd = $selectProducto.value;
+
+  switch (valorProd) {
+    case "Remera Algodon":
+      cortar(listaTalles, 0, 6, $selectTalle);
+      break;
+    case "Remera Poliester":
+      cortar(listaTalles, 0, 6, $selectTalle);
+      break;
+    case "Remera Manga Larga":
+      cortar(listaTalles, 0, 6, $selectTalle);
+      break;
+    case "Remera Bordada":
+      cortar(listaTalles, 0, 6, $selectTalle);
+      break;
+    case "Gorra Azul":
+      cortar(listaTalles, 0, 6, $selectTalle);
+      break;
+    case "Gorra Naranja":
+      cortar(listaTalles, 0, 6, $selectTalle);
+      break;
+    case "Gorra Negra":
+      cortar(listaTalles, 0, 6, $selectTalle);
+      break;
+    case "Gorra Blanca":
+      cortar(listaTalles, 0, 6, $selectTalle);
+      break;
+    case "Taza Blanca":
+      cortar(listaTalles, 6, 7, $selectTalle);
+      break;
+    case "Taza Negra":
+      cortar(listaTalles, 6, 7, $selectTalle);
+      break;
+    case "Taza Naranja":
+      cortar(listaTalles, 6, 7, $selectTalle);
+      break;
+    case "Taza Azul":
+      cortar(listaTalles, 6, 7, $selectTalle);
+      break;
+    case "Sticker Auto":
+      cortar(listaTalles, 6, 7, $selectTalle);
+      break;
+    case "Sticker Termo":
+      cortar(listaTalles, 6, 7, $selectTalle);
+      break;
+    case "Sticker Mochila":
+      cortar(listaTalles, 6, 7, $selectTalle);
+      break;
+    case "Sticker Parche":
+      cortar(listaTalles, 6, 7, $selectTalle);
+      break;
+
+  }
+});
+
+$selectTalle.addEventListener("change", function () {
+  const valorTalle = $selectTalle.value;
+
+  switch (valorTalle) {
+    case "XS":
+      cortar(listaPrecios, 0, 2, $selectPrecio);
+      break;
+    case "S":
+      cortar(listaPrecios, 2, 4, $selectPrecio);
+      break;
+    case "M":
+      cortar(listaPrecios, 4, 6, $selectPrecio);
+      break;
+    case "L":
+      cortar(listaPrecios, 4, 6, $selectPrecio);
+      break;
+    case "XL":
+      cortar(listaPrecios, 6, 8, $selectPrecio);
+      break;
+    case "XXl":
+      cortar(listaPrecios, 6, 8, $selectPrecio);
+      break;
+    case "N/NA":
+      cortar(listaPrecios, 7, 8, $selectPrecio);
+      break;
+  }
+});
 
 function mostrarLocalStorage() {
-  let objObtenido = localStorage.getItem("PRODUCTOS_COMPRA");
+  let objObtenido = localStorage.getItem("Compra")
   if (objObtenido) {
-    console.log(objObtenido);
+    console.log(JSON.parse(objObtenido));
   } else {
     console.log("No hay productos guardados");
   }
 }
 
-mostrarLocalStorage();
-
-let submitButton = document.querySelector("#submitBtn");
-submitButton.addEventListener("click", (event) => {
-  event.preventDefault();
-  mostrarValoresForm();
-});
-
 function mostrarValoresForm() {
-  //
-  const onlyInputs = document.querySelectorAll("#carrito-compra-form select");
-  let objResult = {
-    producto: {},
-    talle: {},
-    precio: {},
-  };
-  onlyInputs.forEach((input, i) => {
-    if (i === 0) {
-      objResult.producto = productos.find((_producto, index) => {
-        return index === input.selectedIndex - 1;
-      });
-    }
-    if (i === 1) {
-      objResult.talle = talles.find((_talle, index) => {
-        return index === input.selectedIndex - 1;
-      });
-    }
-    if (i === 2) {
-      objResult.precio = precios.find((_precio, index) => {
-        return index === input.selectedIndex - 1;
-      });
-    }
-  });
-  localStorage.setItem("PRODUCTOS_COMPRA", JSON.stringify(objResult));
+  function Compra(producto, talle, precio) {
+    this.producto = producto;
+    this.talle = talle;
+    this.precio = precio;
+  }
+  const productoElegido = document.getElementById("selecProd").value;
+  const talleElegido = document.getElementById("selecTalle").value;
+  const precioElegido = document.getElementById("selecPrecio").value;
+  const nuevaCompra = new Compra(productoElegido, talleElegido, precioElegido);
+  const compraLocal = localStorage.setItem("Compra", JSON.stringify(nuevaCompra));
+  document.getElementById("objFinal").innerHTML += `<p>${nuevaCompra.producto}</p>-
+                                                    <p>${nuevaCompra.talle}</p>-
+                                                    <p>${nuevaCompra.precio}</p>`;
 }
-
-let submit = document.querySelector("#submitBtn");
-submit.addEventListener("click", (event) => {
-  event.preventDefault();
-
-  let msj = "Compra Finalizada";
-  document.querySelector("#compraFinal").innerHTML = msj;
-  let objSelec = localStorage.getItem("PRODUCTOS_COMPRA");
-  document.querySelector("#objFinal").innerHTML = objSelec;
-});
-
-productos.forEach(
-  (prod) =>
-    (selecProd.innerHTML += `<option value="${prod}">${prod.nombre}</option>`)
-);
-
-talles.forEach(
-  (talle) =>
-    (selecTalle.innerHTML += `<option value="${talle}">${talle.talle}</option>`)
-);
-
-precios.forEach(
-  (precio) =>
-    (selecPrecio.innerHTML += `<option value="${precio}">${precio.precio}</option>`)
-);
